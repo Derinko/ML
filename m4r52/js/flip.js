2fc3afef627497be3b30d2ee040f00d6b99719b8
Derinko <derickpaula2@gmail.com>
2020-07-25 16:47:50 -0400

    Cambiar ubicacion

--- m4r52/js/flip.js
+++
@@ -1,8 +0,0 @@
-var flip =getElementById('flip') 
-function hover(flip) {
-  flip.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
-}
-
-function unhover(flip) {
-  flip.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
-}
